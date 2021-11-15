6fbb64e7fa4a091ef747cb455b1895fa863b3033
Victor <45384062+victor234256@users.noreply.github.com>
2021-11-15 21:46:18 +0100

    Add files via upload

---
+++ septSet/test.php
@@ -0,0 +1,22 @@
+<?php
+
+// your code goes here.
+$food = "rice"; // string
+$number = 12; // Integer
+$Number = 12.5; // float 
+$food == $number;// false
+$food == "rice";
+
+
+//echo $number;
+//echo(sqrt(64));
+echo(rand(10, 100));
+
+
+
+
+
+
+
+
+?>
